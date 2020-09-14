7606f556494242d9177212fc371b122d7f261679
Aakash Tyagi <aakash.tyg93@gmail.com>
2020-09-14 21:48:15 +0530

    Add Sidebar HTML part

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 
 class Sidebar extends Component {
   render() {
-    return <div>Sidebar</div>;
+    return (
+      <div id="sidebar">
+        <div className="user-profile">
+          <div>
+            <img />
+          </div>
+          <div>Ronald</div>
+        </div>
+
+        <div className="user-channels"></div>
+      </div>
+    );
   }
 }
 
