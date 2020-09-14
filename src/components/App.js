a7c7c87474490cec1c7342d11e68f403e4925e61
Aakash Tyagi <aakash.tyg93@gmail.com>
2020-09-14 21:32:21 +0530

    Add routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { SignIn } from './';
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function Some() {
+  return <div>Some</div>;
+}
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SignIn />
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/some" component={Some} />
+        </Switch>
       </div>
     );
   }
