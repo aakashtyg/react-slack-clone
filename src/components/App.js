8920adc95ecfe9b4f73b5cbd5375102df4297882
Aakash Tyagi <aakash.tyg93@gmail.com>
2020-09-15 16:32:14 +0530

    Add private route

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
 import React, { useContext } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { SignIn, Slack } from './';
 import { UserContext } from '../providers/UserProvider';
+
+const PrivateRoute = ({ component: Component, isLoggedIn, ...others }) => {
+  return (
+    <Route
+      {...others}
+      render={(props) => {
+        return isLoggedIn ? (
+          <Component {...props} />
+        ) : (
+          // we will pass the path the user is trying to access to our login component
+          // instead of passing string we pass object
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: {
+                from: props.location,
+              },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
 
 function App() {
   const auth = useContext(UserContext);
@@ -16,7 +40,12 @@ function App() {
       <Switch>
         <Route exact path="/login" component={SignIn} />
         <Route exact path="/signup" component={SignIn} />
-        <Route exact path="/" component={Slack} />
+        <PrivateRoute
+          exact
+          path="/"
+          component={Slack}
+          isLoggedIn={auth.user ? true : false}
+        />
         {/* <Route exact path="/slack" component={Slack} /> */}
       </Switch>
     </div>
